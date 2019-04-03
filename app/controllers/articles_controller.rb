e3e5099c919b3ebb47a08da5a8ebd7efea5abafe
Paul <islandertron@gmail.com>
2019-04-03 10:17:44 -0400

    Added Update & Delete in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,30 @@
 class ArticlesController < ApplicationController
+    # URI Pattern:  /articles(.format)    articles:index
+    # Controller:   article#index
+    def index
+        @articles = Article.all
+    end
+
+    # URI Pattern:  /article/:id
+    # Controller:   article#show        
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    # URI Pattern:  /articles/new
+    # Controller:   articles#new
     def new 
         @article = Article.new 
     end
 
+    # URI Pattern:  /articles/:id/edit
+    # Controller:   articles#edit
+    def edit
+        @article = Article.find(params[:id])
+    end 
+
+    # URI Pattern:  /articles
+    # Controller:   articles#create
     # Save the data in the db
     def create
         # render plain: params[:article].inspect
@@ -18,21 +40,26 @@ class ArticlesController < ApplicationController
         end 
     end
 
-    # Once form is submitted, it will redirect to: 
-    # localhost:[port]/articles/[id]
-    # /articles/:id(.:format)       articles#show
-    # def show
-    #     @article = Article.find(params[:id])
-    # end
-
-    # /articles(.format)    articles:index
-    def index
-        @articles = Article.all
-    end
-
-    def show
+    # URI Pattern:  /articles/:id
+    # Controller:   articles#update
+    def update
         @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else 
+            render 'edit'
+        end
     end
+
+    # URI Pattern:  /articles/:id
+    # Controller:   articles#destroy
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+       
+        redirect_to articles_path
+      end
 
     private
         def article_params
